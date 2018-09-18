a662cf5d09b5ae0b2b3a8730251ae61a873627a2
vnaumov <vnaumov@mirantis.com>
2018-09-18 16:35:09 +0400

    simplify pylint image

--- py2-alpine/setup.sh
+++ py2-alpine/setup.sh
@@ -4,7 +4,7 @@ set -e
 CMD="$1"
 
 if [ "$CMD" = "pylint" ]; then
-  find -L ./ -name [eE]xtensions -prune -o -name skins -prune -o -name interfaces.py -prune -o -name "*.py" -print | xargs pylint
+  find -L ./ -name "*.py" -print | xargs pylint
 else
   exec "$@"
 fi
